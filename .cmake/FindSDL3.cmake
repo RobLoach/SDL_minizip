3751ce9dfde9c47f50b550ff8859af512308bbaa
Rob Loach <robloach@gmail.com>
2026-07-11 16:00:54 -0400

    Update CMake (#22)

---
+++ .cmake/FindSDL3.cmake
@@ -0,0 +1,14 @@
+# SDL3
+# https://github.com/libsdl-org/SDL
+
+set(SDL_STATIC TRUE)
+set(SDL_SHARED FALSE)
+set(SDL_DISABLE_UNINSTALL TRUE)
+
+include(FetchContent)
+FetchContent_Declare(
+    SDL3Source
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG release-3.4.12
+)
+FetchContent_MakeAvailable(SDL3Source)
